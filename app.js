ae52ccf4f8b0c45c3f2b968ee8bf9e7cdc23ac89
AdamGolan69 <52522249+AdamGolan69@users.noreply.github.com>
2020-04-04 18:59:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const server = require('express')();
-server.use(require('express').json());
-server.get('/', (req, res) => res.sendFile('index.html'));
-server.listen(3000, () => console.log('listening'));
+const express = require('express'),
+    path = require('path'),
+    server = express();
+server.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+server.listen(process.env.PORT || 3000, () => console.log('listening'));
