fc3174647465b4b2fe3a265d4c0fe2e20e7eee6a
AdamGolan69 <52522249+AdamGolan69@users.noreply.github.com>
2020-04-04 19:29:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express'),
     path = require('path'),
     server = express();
-server.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+server.get('', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
 server.listen(process.env.PORT || 3000, () => console.log('listening'));
