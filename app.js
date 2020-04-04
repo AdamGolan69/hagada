04d4d77e02311e23892eecff669c247b028972aa
AdamGolan69 <52522249+AdamGolan69@users.noreply.github.com>
2020-04-04 18:24:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const server = require('express')();
 server.use(require('express').json());
-server.get('/', (req, res) => res.sendfile('index.html'));
+server.get('/', (req, res) => res.sendFile('index.html'));
 server.listen(3000, () => console.log('listening'));
